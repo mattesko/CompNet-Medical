81e7d6e33596444003014b0ef7e678ff4625261b
Matthew Lesko-Krleza <mlesko1996@gmail.com>
2020-11-30 10:32:46 -0800

    Update unet training script

--- train_unet.sh
+++ train_unet.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #SBATCH --time=24:0:0 --ntasks=4 --mem=32000M --nodes=1 --gres=gpu:1 --account=def-petersv
+export DISPLAY=unix$DISPLAY
 module load python/3.6
 module load scipy-stack
 
